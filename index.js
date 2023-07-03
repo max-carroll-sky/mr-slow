c12086443fde4bbd6d1e199354fc338dcf49b920
Max Carroll <max.carroll@sky.uk>
2023-07-03 14:34:11 +0100

    improved log messages

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ const MILI = 1000;
 
 app.get("/wait/:waitSeconds", (req, res) => {
   const timeToWait = req.params.waitSeconds;
-  globalThis.setTimeout(
-    () =>
-      res.send(
-        `Well that was worth waiting for, all ${timeToWait} seconds of it`
-      ),
-    timeToWait * MILI
-  );
+  const { url, ip, method } = req;
+
+  const logInfo = `${ip} ${method} ${url}`;
+
+  console.log(`${logInfo} Request Started...`);
+  globalThis.setTimeout(() => {
+    res.send(`Sent a response with a ${timeToWait} seconds delay`);
+    console.log(`${logInfo} Response Sent`);
+  }, timeToWait * MILI);
 });
 
 app.listen(port, () => {
